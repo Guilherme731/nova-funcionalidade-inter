028aa998e1e8d5a066c89b60ab35e95cd4c3bc21
Guilherme731 <guilherme_moreira6@estudante.sesisenai.org.br>
2025-05-03 22:47:27 -0300

    Atualizacao dinamica dos dados do mes

--- scripts/controleDadosFinanceiros.js
+++ scripts/controleDadosFinanceiros.js
@@ -1,21 +1,35 @@
-// Movimentação de dinheiro em cada mês (não é o saldo final, apenas o que aumentou ou diminuiu)
-const dinheiroMovimentadoMeses = [802.23, 361.14, -330.78, 272.43, -66.32, 300]; // Ordem decrescente (0 = mês atual, 1 = mês anterior ...)
+const depositosMensais = [700, 350, 0, 270, 10, 300]; //Ordem decrescente (0 = mes atual)
+const saquesMensais = [100, 0, 330, 0, 70, 0]; //Ordem decrescente (0 = mes atual)
 
+const jurosMes = 1.05; //%
+const impostosMes = 10; //%
 
 const mesAtual = 5;
 const anoAtual = '2025';
-const meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
-let saldoMes = [0, 0, 0, 0, 0, 0];
 
-function preencherSaldoMes(){
-    let total = 0;
+const meses = ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'];
+
+// Movimentação de dinheiro em cada mês (não é o saldo final, apenas o que aumentou ou diminuiu)
+let dinheiroMovimentadoMeses = [0, 0, 0, 0, 0, 0]; // Ordem decrescente (0 = mês atual, 1 = mês anterior ...)
+let saldoMes = [0, 0, 0, 0, 0, 0];
+let rendimentos = [0, 0, 0, 0, 0, 0];
+
+function calcularMovimentacoesESaldo(){
+    let saldo = 0;
     for(let i = (saldoMes.length - 1); i >= 0; i--){
-        total += dinheiroMovimentadoMeses[i];
-        saldoMes[i] = total;
+        let saldoI = saldo;
+        saldo += depositosMensais[i];
+        saldo -= saquesMensais[i];
+        rendimento = saldo * jurosMes/100;
+        saldo += rendimento;
+        saldo -= rendimento * impostosMes/100; 
+        saldoMes[i] = saldo;
+        dinheiroMovimentadoMeses[i] = saldo - saldoI;
+        rendimentos[i] = rendimento;
     }
 }
 
-preencherSaldoMes();
+calcularMovimentacoesESaldo();
 
 function saldoTotal(){
     let total = 0;
@@ -69,13 +83,49 @@ function atualizarBarrasGrafico(){
 
 }
 
-document.getElementsByClassName('saldoGrande')[0].innerText = 'R$ ' + saldoTotal();
-document.getElementsByClassName('saldoPequeno')[0].innerText = 'R$ ' + saldoTotal();
+document.getElementsByClassName('saldoGrande')[0].innerText = 'R$ ' + saldoTotal().toFixed(2);
+document.getElementsByClassName('saldoPequeno')[0].innerText = 'R$ ' + saldoTotal().toFixed(2);
 preencherDadosGrafico();
 atualizarBarrasGrafico();
 atualizarDadosMensais(0);
 
+function obterMes(mes){
+    if(mes >= 0){
+        return meses[mes];
+    }else{
+        return meses[12 - (mes * -1)];
+    }
+}
 
 function atualizarDadosMensais(id){
-    
+    const titulo = document.getElementById('tituloDadosMes');
+    const saldoInicial = document.getElementById('saldoInicialMes');
+    const saldoFinal = document.getElementById('saldoFinalMes');
+    const imgVariacao = document.getElementById('imgVariacaoMes');
+    const variacaoMes = document.getElementById('variacaoMes');
+    const depositos = document.getElementById('totalDepositos');
+    const saques = document.getElementById('totalSaques');
+    const imgRendimento = document.getElementById('imgRendimento');
+    const rendimento = document.getElementById('rendimento');
+    const impostos = document.getElementById('impostos');
+
+    titulo.innerText = 'Dados do mês ' + obterMes(mesAtual - id - 1);
+    saldoInicial.innerText = 'R$ ' + (saldoMes[id] - dinheiroMovimentadoMeses[id]).toFixed(2);
+    saldoFinal.innerText = 'R$ ' + saldoMes[id].toFixed(2);
+    variacaoMes.innerText = 'R$ ' + dinheiroMovimentadoMeses[id].toFixed(2);
+    if(dinheiroMovimentadoMeses[id] >= 0){
+        imgVariacao.setAttribute('src', 'assets/icons/setaCima.png');
+    }else{
+        imgVariacao.setAttribute('src', 'assets/icons/setaBaixo.png');
+    }
+    depositos.innerText = 'R$ ' + depositosMensais[id].toFixed(2);
+    saques.innerText = 'R$ ' + saquesMensais[id].toFixed(2);
+    rendimento.innerText = 'R$ ' + rendimentos[id].toFixed(2);
+    if(rendimentos[id] >= 0){
+        imgRendimento.setAttribute('src', 'assets/icons/setaCima.png');
+    }else{
+        imgRendimento.setAttribute('src', 'assets/icons/setaBaixo.png');
+    }
+    impostos.innerText = 'R$ ' + (rendimentos[id] * (impostosMes/100)).toFixed(2);
+
 }
